8bffcce7391f38935be33c8602f27ff124e2e08b
Mohd Ahshan Danish <mailtodanish@gmail.com>
2022-11-24 22:21:17 +0530

    bug: mobile enabled

--- script.js
+++ script.js
@@ -2,17 +2,18 @@
  * author : mohd ahshan danish
  * 
  */
+
+function playSound(e) {
+    const el = document.querySelector(`div[data-key="${e.which}"]`);
+    if (!el) return;
+    el.classList.add('play');
+    const a = document.querySelector(`audio[data-key="${e.which}"]`);
+    a.currentTime = 0;
+    a.play();
+}
+
 (() => {
-    document.addEventListener("keydown", (e) => {
-        const el = document.querySelector(`div[data-key="${e.which}"]`);
-        if (!el) return;
-
-        el.classList.add('play');
-
-        const a = document.querySelector(`audio[data-key="${e.which}"]`);
-        a.currentTime=0;
-         a.play();
-    });
+    document.addEventListener("keydown", playSound);
 
     const keys = document.querySelectorAll('.key');
     keys.forEach((k) => {
@@ -21,6 +22,10 @@
                 k.classList.remove("play");
             }
 
-        })
+        });
+
+        k.addEventListener("click", (e) => {
+            playSound({ "which": e.target.getAttribute('data-key') });
+        });
     });
 })();
